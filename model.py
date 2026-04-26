f3f6355f27bcbb74bcd1ad77d4568f5f3ed176c4
aportra <aportra99@gmail.com>
2026-04-26 13:36:25 -0700

    updated model.py to use discord as ui

--- model.py
+++ model.py
@@ -1,265 +1,105 @@
-#!/home/aportra99/spinach-rag/ai/bin/python
 import ollama
+# import os
+import discord
+import asyncio
 import yaml
-import re
-import sys
-from spinach import look
+import draw
+import bot_commands
+from psql import psql
+# from spinach import look
 from spinach import req_news
 from spinach import search
-from pathlib import Path
 from datetime import datetime, timedelta
+from pathlib import Path
 
-us_sources = [
-"abc-news",
-"associated-press",
-"axios",
-"breitbart-news",
-"bloomberg",
-"business-insider",
-"cbs-news",
-"cnbc",
-"cnn",
-"espn",
-"financial-times",
-"fox-news",
-"fox-sports",
-"msnbc",
-"nbc-news",
-"nbc-sports",
-"politico",
-"reuters",
-"the-hill",
-"the-wall-street-journal",
-"the-washington-post",
-"techcrunch",
-"usa-today"
-]
-yesterday = datetime.now() - timedelta(1)
 home = Path.home()
 try:
     with open(f'{home}/spinach/rag-parsing/config.yaml', 'r') as f:
         data = yaml.load(f, Loader=yaml.SafeLoader)
-    model = data.get("model")
 except FileNotFoundError:
     with open(f'{home}/spinach/rag-parsing/config-default.yaml', 'r') as f:
         data = yaml.load(f, Loader=yaml.SafeLoader)
-messages = []
+model = data.get("model")
+discord_key = data.get("discord_key")
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
 messages = [
     {
         "role": "system",
         "content": (
             "You're a helpful assistant"
+            "Make sure the message will fit in discord rules of 4000 words or less"
+            f"Current date is {datetime.now()}"
         ),
     }
 ]
+max_history = 20
+schema_messages = []
 
-while True:
-    prompt = input('>>')
-    print('')
-    if prompt == '+++':
-        while True:
-            text = sys.stdin.readline()
-            if text.strip().endswith('END'):
-                break
-            else:
-                prompt += text
-    if prompt.strip().split()[0].lower() == 'look':
-        try:
-            if prompt.strip().split()[1].lower() == 'dyn':
-                path_loaded = None
-                file_output = look(context=prompt, folder='dynamic')
-                file_loaded = file_output[0]
-                top_idx = file_output[1]
-                prompt = file_output[2]
-                if file_output is None:
-                    continue
-                else:
-                    message_return = {"role":"user","content": f"Here is the content of the file \n```\n{file_output[0][top_idx]['content']}\n```"}
-            elif prompt.strip().split()[1].lower() == 'data':
-                path_loaded = None
-                file_output = look(context=prompt, folder='data')
-                print(file_output)
-                file_loaded = file_output[0]
-                top_idx = file_output[1]
-                prompt = file_output[2]
-                if file_output is None:
-                    continue
-                else:
-                    message_return = {"role":"user","content": f"Here is the content of the file \n```\n{file_output[0][top_idx]['content']}\n```"}
-            else:
-                file_output = look(context=prompt,folder=None)
-                path_loaded = prompt.strip().split()[1].lower()
-                file_loaded = file_output[0]
-                top_idx = file_output[1]
-                prompt = file_output[2]
 
-                if file_output is None:
-                    continue
-                else:
-                    message_return = {"role":"user","content": f"Here is the content of the file `{path_loaded}`:\n```\n{file_output[0][top_idx]['content']}\n```"}
-            messages.append(message_return)
-        except TypeError as e:
-            print(e)
-            messages.pop()
-            continue
-        except RuntimeError as e:
-            print(e)
-            messages.pop()
-            continue
-        except FileNotFoundError as e:
-            print(e)
-            messages.pop()
-            continue
-    elif prompt.strip().split()[0].lower() == 'quit' or prompt.strip().split()[0].lower() == 'bye':
-        quit()
-    elif prompt.strip().split()[0].lower() == 'news':
-        news = None
-        if len(prompt.strip().split()) < 2:
-            news = req_news(None, None, None)
-            if not news:
-                print('No news returned, check news api key in config-default.yaml')
-                print('This tool uses NewsAPI  for retrieving news')
-                print('If you do not have an api key you can retrieve one here: https://newsapi.org/')
-                messages.pop()
-                continue
-            else:
-                message_news = ''
-                for key in news:
-                    message_news += key + ' '
-                    for i in range(len(news[key])):
-                        message_news += news[key][i]
-                message_return = {"role": "user",
-                                    "content": f"Here is the content of the news \n```\n{message_news}\n```"}
-                messages.append(message_return)
-                prompt = f'summarize, at the end of each summary supply the url of article. each article should be counted. Example 1. First article 2. Second Article. Articles are from Date: {yesterday}. Summarize them in as much detail as you can while making sure to not make anything up.'
-        elif len(prompt.strip().split()) == 2 and prompt.strip().split()[1] not in us_sources and prompt.strip().split()[1]!='help' and prompt.strip().split()[1]!='search':
-            news = req_news(None, None, int(prompt.strip().split()[1]))
 
-            if not news:
-                print('No news returned, check news api key in config-default.yaml')
-                print('This tool uses NewsAPI  for retrieving news')
-                print('If you do not have an api key you can retrieve one here: https://newsapi.org/')
-                messages.pop()
-                continue
-            else:
-                message_news = ''
-                for key in news:
-                    message_news += key + ' '
-                    for i in range(len(news[key])):
-                        message_news += news[key][i]
-                message_return = {"role": "user",
-                                    "content": f"Here is the content of the news \n```\n{message_news}\n```"}
-                messages.append(message_return)
-                prompt = f'summarize, at the end of each summary supply the url of article. each article should be counted. Example 1. First article 2. Second Article. Articles are from Date: {yesterday}. Summarize them in as much detail as you can while making sure to not make anything up.'
-        elif prompt.strip().split()[1].lower() == 'help':
-            print('You can query specific news sources for their top headlines by prompting "news source"')
-            print('Leaving this blank will query by default associated-press, politico,the-hill, and financial-times')
-            print('These can be adjusted in the config.yaml')
-            print('Below are the currently available news sources')
-            for i in range(len(us_sources)):
-                print(f'{i+1}. {us_sources[i]}')
-            continue
-        elif prompt.strip().split()[1].lower() == 'search':
-            news = None
-            if prompt.strip().split()[2].lower() in us_sources and len(prompt.strip().split()) == 4:
-                news = req_news(prompt.strip().split()[2].lower(), prompt.strip().split()[3].lower(), None)
-            elif prompt.strip().split()[2].lower() in us_sources and len(prompt.strip().split()) == 5:
-                news = req_news(prompt.strip().split()[2].lower(), prompt.strip().split()[3].lower(),int(prompt.strip().split()[4].lower()))
-            elif prompt.strip().split()[2].lower() not in us_sources and len(prompt.strip().split()) == 3:
-                news = req_news(None, prompt.strip().split()[2].lower(), None)
-            elif prompt.strip().split()[2].lower() not in us_sources and len(prompt.strip().split()) == 4:
-                news = req_news(None,prompt.strip().split()[2].lower(),int(prompt.strip().split()[3].lower()))
-            if not news:
-                print('No news returned, check news api key in config-default.yaml')
-                print('This tool uses NewsAPI  for retrieving news')
-                print('If you do not have an api key you can retrieve one here: https://newsapi.org/')
-                messages.pop()
-                continue
-            else:
-                message_news = ''
+def split_message(text, limit=2000):
+    chunks = []
+    while len(text) > limit:
+        split_at = text.rfind('\n', 0, limit)
+        if split_at == -1:
+            split_at = text.rfind(' ', 0, limit)
+            if split_at == -1:
+                split_at = limit  # no whitespace found, hard cut
+        chunks.append(text[:split_at])
+        text = text[split_at:].lstrip()
+    chunks.append(text)
+    return chunks
 
-                for key in news:
-                    message_news += key + ' '
-                    for i in range(len(news[key])):
-                        message_news += news[key][i]
-                message_return = {"role": "user",
-                                    "content": f"Here is the content of the news \n```\n{message_news}\n```"}
-                messages.append(message_return)
 
-                prompt = f'summarize, at the end of each summary supply the url of article. each article should be counted. Example 1. First article 2. Second Article. Articles are from Date: {yesterday}. Summarize them in as much detail as you can while making sure to not make anything up.'
-        else:
-            news = None
-            if len(prompt.strip().split()) != 3:
-                news = req_news(prompt.strip().split()[1].lower(), None, None)
-            elif len(prompt.strip().split()) == 3:
-                news = req_news(prompt.strip().split()[1].lower(), None, int(prompt.strip().split()[2].lower()))
-            if not news:
-                print('No news returned, check news api key in config-default.yaml')
-                print('This tool uses NewsAPI  for retrieving news')
-                print('If you do not have an api key you can retrieve one here: https://newsapi.org/')
-                messages.pop()
-                continue
-            else:
-                message_news = ''
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user} and ready")
 
-                for key in news:
-                    message_news += key + ' '
-                    for i in range(len(news[key])):
-                        message_news += news[key][i]
-                message_return = {"role": "user",
-                                    "content": f"Here is the content of the news \n```\n{message_news}\n```"}
-                messages.append(message_return)
 
-                prompt = f'summarize, at the end of each summary supply the url of article. each article should be counted. Example 1. First article 2. Second Article. Articles are from Date: {yesterday}. Summarize them in as much detail as you can while making sure to not make anything up.'
+@client.event
+async def on_message(message):
+    commands = ({"!set_schema":lambda:bot_commands.set_schema(message.content.split(' ', 1)[1],schema_messages),
+                 "!query":lambda:bot_commands.query(message.content.split(' ', 1)[1], schema_messages),
+                 "!search":lambda:bot_commands.search_fn(message.content.split(' ', 1)[1], messages),
+                 "!news":lambda:bot_commands.news_fn(messages)})
+    if message.channel.id != 1496956603612532766:
+        return
+    if message.author == client.user:
+        return
+    if message.content.startswith("!"):
+        com = message.content.split(' ', 1)[0]
+        if com not in commands.keys():
+            await message.channel.send(f"not valid command:{com}")
+            return
+        buf, response = await commands[com]()
+        if response is not None:
+            chunks = split_message(response)
+            for chunk in chunks:
+                await message.channel.send(chunk)   # messages.append
+            # await message.channel.send(response)
 
-    elif prompt.strip().split()[0].lower() == 'search':
-        search_var = search(prompt.split(' ',1)[1])
-        if not search_var:
-            print('No search results returned, check search_api key in config-default.yaml')
-            print('This tool uses Serper search API for searching google')
-            print('If you do not have an api key you can retrieve one here: https://serper.dev/')
-            messages.pop()
-            continue
-        else:
-            search_results = ''
+        if buf is not None:
+            await message.channel.send(file=discord.File(buf, filename="table.png"))
+            buf.close()
 
-            for key in search_var:
-                search_results += key + ' '
-                for i in range(len(search_var[key])):
-                    search_results += search_var[key][i]
-            message_return = {"role": "user",
-                                "content": f"Here is the content of the search results \n```\n{search_results}\n```"}
-            messages.append(message_return)
+        return
 
-            prompt = f'from the search results given, try your best to answer the question, provide sources, and if you feel as though you cannot discern a proper answer simply return the urls with a summary of each. The question or search was: {prompt.split(' ',1)[1]}'
+    messages.append({"role": "user", "content": message.content})
+    print(f"Message received: {message.content}")
 
-    elif prompt.strip().split()[0].lower() == 'update':
-        if path_loaded is None:
-            print('no valid file loaded')
-            continue
-        else:
-            update_prompt = "look "+ path_loaded
-            print(update_prompt)
-            file_output = look(context=update_prompt,folder=None)
-            file_loaded = file_output[0]
-            top_idx = file_output[1]
-            output = file_output[2]+ f"" + file_output[0][top_idx]['content']
-            message_return = {"role": "user",
-                              "content": f"{file_output[2]} here is the content of the file name {path_loaded}:{file_output[0][top_idx]['content']}"}
-            messages.append(message_return)
-    elif prompt.strip().split()[0].lower() == 'reset':
-        messages.clear()
-        file_loaded.clear()
-        print('context cleared')
-        continue
-    messages.append({"role": "user", "content": prompt})
-    response = (ollama.chat(model=model,
+    response = await asyncio.get_event_loop().run_in_executor(None,lambda:ollama.chat(model=model,
                             messages=messages,
-                            stream=True))
-    ai_response = ''
-    skip = False
-    for i in response:
-        ai_response += i["message"]["content"]
-        print(i["message"]["content"], end='', flush=True)
-    messages.append({"role": "assistant", "content": ai_response})
-    print('')
+                            stream=False))
+    content = response["message"]["content"]
+    messages.append({"role": "assistant", "content": content})
+    if len(messages) > max_history:
+        messages[1:] = messages[-(max_history-1):]
+    chunks = split_message(content)
+
+    for chunk in chunks:
+        await message.channel.send(chunk)   # messages.append
+
+client.run(discord_key)
